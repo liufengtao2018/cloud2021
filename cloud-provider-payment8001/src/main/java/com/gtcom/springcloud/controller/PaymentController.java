f1cefe6d3a96bf73ac33ba086d68f0441fa05ca7
lft <51482842@qq.com>
2021-02-03 15:16:29 +0800

    工程重构

--- cloud-provider-payment8001/src/main/java/com/gtcom/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/gtcom/springcloud/controller/PaymentController.java
@@ -4,10 +4,8 @@ import com.gtcom.springcloud.entities.CommonResult;
 import com.gtcom.springcloud.entities.Payment;
 import com.gtcom.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 
 /**
@@ -23,7 +21,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value="/payment/create")
-    public CommonResult create(Payment payment)
+    public CommonResult create(@RequestBody Payment payment)
     {
         log.info(payment.getSerial());
         int result = paymentService.create(payment);
