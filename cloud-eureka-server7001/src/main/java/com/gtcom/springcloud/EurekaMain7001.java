a9659fd2081388fcbc61674aa507a052dbd7e1b6
lft <51482842@qq.com>
2021-02-03 17:53:39 +0800

    eureka集群构建

--- cloud-eureka-server7001/src/main/java/com/gtcom/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/gtcom/springcloud/EurekaMain7001.java
@@ -2,19 +2,19 @@ package com.gtcom.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @description:
  * @author: morning
- * @createDate: 2021/2/3 13:41
+ * @createDate: 2021/2/3 16:14
  * @version:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80
+@EnableEurekaServer
+public class EurekaMain7001 
 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
--- cloud-eureka-server7002/src/main/java/com/gtcom/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/gtcom/springcloud/EurekaMain7002.java
@@ -2,19 +2,19 @@ package com.gtcom.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @description:
  * @author: morning
- * @createDate: 2021/2/3 13:41
+ * @createDate: 2021/2/3 17:18
  * @version:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80
+@EnableEurekaServer
+public class EurekaMain7002
 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 }
